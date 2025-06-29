6a5e51a34b8f16e34e90b57acab4127763204ab1
dotlabs-gs <germano@dotlabs.com.br>
2025-06-29 18:11:30 -0300

    add sugestion

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -489,6 +489,7 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     imageUrl: Schema.Attribute.String;
     isActive: Schema.Attribute.Boolean;
     isPromotion: Schema.Attribute.Boolean;
+    isSugestion: Schema.Attribute.Boolean;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
       'oneToMany',
