4c00309fc080228f626b90ecc9a0cc26cb36f2fe
dotlabs-gs <germano@dotlabs.com.br>
2025-07-14 19:24:37 -0300

    adds segmentation on product

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -587,6 +587,10 @@ export interface ApiProductProduct extends Struct.CollectionTypeSchema {
     price: Schema.Attribute.BigInteger;
     publishedAt: Schema.Attribute.DateTime;
     restaurantId: Schema.Attribute.String;
+    segmentations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::segmentation.segmentation'
+    >;
     suggestions: Schema.Attribute.Relation<'oneToMany', 'api::product.product'>;
     tags: Schema.Attribute.Relation<'oneToMany', 'api::tag.tag'>;
     updatedAt: Schema.Attribute.DateTime;
